0a419f9d30c314bdd845417db0ae71d2a4294fe5
gdessard <github@dessard.fr>
2015-06-22 14:20:56 +0200

    Gestion affichage du RSS. OK pour l'affichage. Reste à gérer les images, lien vers les articles etc...

--- app/src/main/java/com/allezsedan/sedanfootball/MainActivity.java
+++ app/src/main/java/com/allezsedan/sedanfootball/MainActivity.java
@@ -1,32 +1,40 @@
 package com.allezsedan.sedanfootball;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.xml.sax.SAXException;
-import org.w3c.dom.Document;
+import com.allezsedan.sedanfootball.bean.news.Channel;
+import com.allezsedan.sedanfootball.bean.news.Item;
+import com.allezsedan.sedanfootball.persistance.Rss2Parser;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refrech();
 
     }
 
@@ -60,9 +68,51 @@ public class MainActivity extends AppCompatActivity {
      * Read http://www.allezsedan.com/sedanRss.xml
      */
     private void refrech() {
+        LinearLayout gridLayoutItemRss = (LinearLayout) findViewById(R.id.linearLayout_item_list);
+        gridLayoutItemRss.removeAllViewsInLayout();
+        TextView titreRssTextView = (TextView) findViewById(R.id.titre_rss);
 
-        TextView accueilView =(TextView) findViewById(R.id.text_view_accueil);
-        accueilView.setText("fsdf");
+        Rss2Parser rss2Parser = new Rss2Parser();
+        // convert String into InputStream
+        InputStream in = new ByteArrayInputStream(rssString.getBytes());
+        Channel channel;
+        try {
+            channel = rss2Parser.parse(in);
+        } catch (XmlPullParserException e) {
+            Log.e(TAG, "Erreur lecture RSS (XmlPullParserException)", e);
+            titreRssTextView.setText("Erreur lecture RSS");
+            return;
+        } catch (IOException e) {
+            Log.e(TAG, "Erreur lecture RSS (IOException)", e);
+            titreRssTextView.setText("Erreur lecture RSS");
+            return;
+        }
+
+        //Entete RSS
+        titreRssTextView.setText(channel.getTitle());
+
+        //Gestion items
+        List<Item> itemList = channel.getItemList();
+        for (Item item : itemList) {
+            //Récupération du gridLayout d'un item du RSS
+            LayoutInflater inflater = this.getLayoutInflater();
+            View itemView = inflater.inflate(R.layout.item_rss, null);
+
+            TextView titreItemRss = (TextView) itemView.findViewById(R.id.titre_item_rss);
+            titreItemRss.setText(item.getTitle());
+
+            //Ajout description
+            TextView textViewDescription = (TextView) itemView.findViewById(R.id.description_item_rss);
+            textViewDescription.setText(item.getDescription());
+            //Ajout date
+            TextView textViewDate = (TextView) itemView.findViewById(R.id.date_item_rss);
+            DateFormat formatter = new SimpleDateFormat("dd/MMM/yyyy HH:mm");
+            String pubDateFormat = formatter.format(item.getPubDate());
+            textViewDate.setText(pubDateFormat);
+
+            gridLayoutItemRss.addView(itemView);
+
+        }
 
 
         /**
@@ -96,5 +146,47 @@ public class MainActivity extends AppCompatActivity {
 
          */
     }
+
+    /**
+     * Just for test parsing. Replace by call to http://www.allezsedan.com/sedanRss.xml
+     * FIXME to delete, juste for test
+     */
+    public final String rssString =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                    "\t<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n" +
+                    "    <channel>\n" +
+
+                    "      <link>http://www.allezsedan.com</link>\n" +
+
+                    "      <description>allezSedan.com site non officiel du cssa :: site des supporters !</description>\n" +
+                    "      <language>fr-fr</language>\n" +
+                    "\t  <copyright>Copyright 2005 AllezSedan.com </copyright>\n" +
+                    "\t  <ttl>1</ttl>\n" +
+                    "\t  <image>\n" +
+                    "\t\t<title>www.AllezSedan.com : premier site RSS Sedanais du net</title>\n" +
+                    "\t\t<url>http://www.allezsedan.com/images/logoAllezSedan.gif</url>\n" +
+                    "        <link>http://www.allezsedan.com</link>\n" +
+                    "      </image>\n" +
+                    "      <lastBuildDate>Mon, 01 Jun 2015 18:45:34 +0200</lastBuildDate>\n" +
+                    "\t\t  \n" +
+                    "\t<item>\n" +
+                    "\t\t<title>AAAAAU17 DH, Reims - Sedan, les photos</title>\n" +
+                    "\t\t<pubDate>Mon, 01 Jun 2015 18:41:41 +0200</pubDate>\n" +
+                    "\t\t<description>Les photos de la rencontre sont en ligne sur le site, espace multimédia. </description>\n" +
+                    "\t\t<link>http://www.allezsedan.com/article5958_U17_DH_Reims_-_Sedan_les_photos.html</link>\n" +
+                    "\t\t<guid>http://www.allezsedan.com/article5958_U17_DH_Reims_-_Sedan_les_photos.html</guid>\n" +
+                    "\t\t<enclosure url='http://www.allezsedan.com/images/logoAllezSedan.jpg' length='26204' type='image/jpeg' />\n" +
+                    "\t</item>\n" +
+                    "\t<item>\n" +
+                    "\t\t<title>Sedan - Lille B, les images du match</title>\n" +
+                    "\t\t<pubDate>Mon, 25 May 2015 10:14:48 +0200</pubDate>\n" +
+                    "\t\t<description>description sdmlfjsdml fjsdml fjmlsdfj mlsdfj mlsdj fmlsdqfjsq</description>\n" +
+                    "\t\t<link>http://www.allezsedan.com/article5957_Sedan_-_Lille_B_les_images_du_match.html</link>\n" +
+                    "\t\t<guid>http://www.allezsedan.com/article5957_Sedan_-_Lille_B_les_images_du_match.html</guid>\n" +
+                    "\t\t<enclosure url='http://www.allezsedan.com/ftp/article/mini/DSC_8973.jpg' length='11574' type='image/jpeg' />\n" +
+                    "\t</item>\n" +
+
+                    "\t</channel>\n" +
+                    "    </rss>";
 
 }
